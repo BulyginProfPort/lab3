9150d6661ba640e5ee3ea52c97a8ad80bb22ec34
Fedor <fedya.bulygin@mail.ru>
2022-05-17 22:01:48 +0300

    Initial Commit

---
+++ lab3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  lab3
+//
+//  Created by Фёдор Булыгин on 17.05.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
